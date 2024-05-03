8afed64a76e74947ef10ba79f6aa0031cd989851
Kin Wai Lau <kinwailau6-c@my.cityu.edu.hk>
2024-05-03 16:09:28 +0800

    remove unused file

--- run_train_speech_cmd_v2.sh
+++
@@ -1,8 +0,0 @@
-export PYTHONPATH=/data1/steven/audio/AudioRepInceptionNeXt:$PYTHONPATH
-
-CUDA_VISIBLE_DEVICES=0,1,2,3 python tools/run_net.py --cfg configs/SPEECH-CMD-V2/AudioRepInceptionNeXt.yaml --init_method tcp://localhost:9996 \
-NUM_GPUS 4 \
-OUTPUT_DIR /data1/steven/audio/AudioRepInceptionNeXt/checkpoints_speech_cmd/AudioRepInceptionNeXt \
-SPEECHCOMMANDV2.AUDIO_DATA_FILE /data_ssd/DATA/Speech-Command-V2 \
-SPEECHCOMMANDV2.ANNOTATIONS_DIR  /data1/steven/audio/AudioRepInceptionNeXt \
-TRAIN.CHECKPOINT_FILE_PATH /data1/steven/audio/AudioRepInceptionNeXt/checkpoints_vgg/AudioRepInceptionNeXt/checkpoints/checkpoint_epoch_00050.pyth
